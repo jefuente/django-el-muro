d4faca9c05023800dab6603ca5b39660a6e980e8
Julio Fuentes <jefuente@uc.cl>
2021-09-19 22:44:33 -0300

    Actualizacion

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('registro', auth.registro),
     path('login/', auth.login),
     path('logout', auth.logout),
-
     path('mensaje/crear', views.mensaje_crear),
     path('mensaje/comentario', views.comentario_crear),
     path('comentario/<int:val>/borrar', views.comentario_borrar),
